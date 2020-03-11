dfbec88bcb340366fa9b0868654ef99c4cac914c
Keri Levesque <kerianne166@gmail.com>
2020-03-11 12:01:02 -0400

    finished

--- isTwinPrime.playground/Contents.swift
+++ isTwinPrime.playground/Contents.swift
@@ -1,6 +1,33 @@
 import UIKit
 
+func isTwinPrime(_ value: Int) -> Bool {
+    if isPrime(value){
+        if isPrime(value + 2) || isPrime(value - 2) {
+            return true
+        }
+    }
+    
+    return false
+}
 
+func isPrime(_ value: Int) -> Bool {
+    if value <= 3 {
+        return value > 1
+    } else if value % 2 == 0 || value % 3 == 0 {
+        return false
+    }
+    
+    var i = 5
+    
+    while i * i <= value {
+        if value % i == 0 || value % (i + 2) == 0 {
+            return false
+        }
+        i += 6
+    }
+    
+    return true
+}
 
 
 
@@ -17,14 +44,14 @@ import UIKit
 
 
 //Test cases
-//isTwinPrime(2)   // false
-//isTwinPrime(10)  // false
-//isTwinPrime(9)   // false
-//isTwinPrime(4)   // false
-//isTwinPrime(5)   // true
-//isTwinPrime(7)   // true
-//isTwinPrime(23)  // false
-//isTwinPrime(37)  // false
-//isTwinPrime(883) // true
-//isTwinPrime(907) // false
-//isTwinPrime(169) // false
+isTwinPrime(2)   // false
+isTwinPrime(10)  // false
+isTwinPrime(9)   // false
+isTwinPrime(4)   // false
+isTwinPrime(5)   // true
+isTwinPrime(7)   // true
+isTwinPrime(23)  // false
+isTwinPrime(37)  // false
+isTwinPrime(883) // true
+isTwinPrime(907) // false
+isTwinPrime(169) // false
